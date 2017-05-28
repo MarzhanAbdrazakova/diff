6748b910319da54b89782f057ac18f02b9428a63
MarzhanAbdrazakova <atmarzhan@gmail.com>
2017-05-28 21:18:49 +0300

    Update rest_forward.py

--- rest_forward.py
+++ rest_forward.py
@@ -27,3 +27,23 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.ofproto import ofproto_v1_3_parser
 from ryu.ofproto import ether
 from ryu.ofproto import inet
+
+# About forward rules:
+# get rules of all port forwards 
+# * for no vlan
+# GET /forward/rules/{switch-id}
+# * for specific vlan group
+# GET /forward/rules/{switch-id}/{vlan-id}
+# set a rule to the specific switch
+# * for no vlan
+# POST /forward/rules/{switch-id}
+# * for specific vlan group
+# POST /forward/rules/{switch-id}/{vlan-id}
+# delete a rule of port forward of the specific switch from ruleID
+# * for no vlan
+# DELETE /forward/rules/{switch-id}
+# * for specific vlan group
+# DELETE /forward/rules/{switch-id}/{vlan-id}
+# request body format:
+# {"<field>":"<value>"}
+# "rule_id" : "<int>" or "all"
