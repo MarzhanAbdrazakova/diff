d64880554100beed81f7d98e13ca1b6941ab7dfe
MarzhanAbdrazakova <atmarzhan@gmail.com>
2017-06-04 01:06:00 +0300

    Update rest_forward.py

--- rest_forward.py
+++ rest_forward.py
@@ -113,20 +113,351 @@ REQUIREMENTS = {'switchid': SWITCHID_PATTERN,
 
 LOG = logging.getLogger(__name__)
 
+class RestForwardAPI(app_manager.RyuApp):
+
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION,
+                    ofproto_v1_2.OFP_VERSION,
+                    ofproto_v1_3.OFP_VERSION]
+
+    _CONTEXTS = {
+        'dpset': dpset.DPSet,
+        'conf_switch': conf_switch.ConfSwitchSet,
+        'wsgi': WSGIApplication}
+
+    def __init__(self, *args, **kwargs):
+        super(RestForwardAPI, self).__init__(*args, **kwargs)
+
+        # logger configure
+        ForwardController.set_logger(self.logger)
+        self.cs = kwargs['conf_switch']
+        self.dpset = kwargs['dpset']
+        wsgi = kwargs['wsgi']
+        self.waiters = {}
+        self.data = {}
+        self.data['dpset'] = self.dpset
+        self.data['waiters'] = self.waiters
+        wsgi.registory['ForwardController'] = self.data
+        wsgi.register(ForwardController, self.data)
+
+    def stats_reply_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+
+        if dp.id not in self.waiters:
+            return
+        if msg.xid not in self.waiters[dp.id]:
+            return
+        lock, msgs = self.waiters[dp.id][msg.xid]
+        msgs.append(msg)
+
+        flags = 0
+        if dp.ofproto.OFP_VERSION == ofproto_v1_0.OFP_VERSION or \
+                dp.ofproto.OFP_VERSION == ofproto_v1_2.OFP_VERSION:
+            flags = dp.ofproto.OFPSF_REPLY_MORE
+        elif dp.ofproto.OFP_VERSION == ofproto_v1_3.OFP_VERSION:
+            flags = dp.ofproto.OFPMPF_REPLY_MORE
+
+        if msg.flags & flags:
+            return
+        del self.waiters[dp.id][msg.xid]
+        lock.set()
+
+    @set_ev_cls(conf_switch.EventConfSwitchSet)
+    def conf_switch_set_handler(self, ev):
+        if ev.key == cs_key.OVSDB_ADDR:
+            ForwardController.set_ovsdb_addr(ev.dpid, ev.value)
+        else:
+            ForwardController._LOGGER.debug("unknown event: %s", ev)
+
+    @set_ev_cls(conf_switch.EventConfSwitchDel)
+    def conf_switch_del_handler(self, ev):
+        if ev.key == cs_key.OVSDB_ADDR:
+            ForwardController.delete_ovsdb_addr(ev.dpid)
+        else:
+            ForwardController._LOGGER.debug("unknown event: %s", ev)
+
+    @set_ev_cls(dpset.EventDP, dpset.DPSET_EV_DISPATCHER)
+    def handler_datapath(self, ev):
+        if ev.enter:
+            ForwardController.regist_ofs(ev.dp, self.CONF)
+        else:
+            ForwardController.unregist_ofs(ev.dp)
+
+    # for OpenFlow version1.0
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def stats_reply_handler_v1_0(self, ev):
+        self.stats_reply_handler(ev)
+
+    # for OpenFlow version1.2 or later
+    @set_ev_cls(ofp_event.EventOFPStatsReply, MAIN_DISPATCHER)
+    def stats_reply_handler_v1_2(self, ev):
+        self.stats_reply_handler(ev)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        ForwardController.packet_in_handler(ev.msg)
+
+
+class ForwardOfsList(dict):
+
+    def __init__(self):
+        super(ForwardOfsList, self).__init__()
+
+    def get_ofs(self, dp_id):
+        if len(self) == 0:
+            raise ValueError('forward sw is not connected.')
+
+        dps = {}
+        if dp_id == REST_ALL:
+            dps = self
+        else:
+            try:
+                dpid = dpid_lib.str_to_dpid(dp_id)
+            except:
+                raise ValueError('Invalid switchID.')
+
+            if dpid in self:
+                dps = {dpid: self[dpid]}
+            else:
+                msg = 'forward sw is not connected. : switchID=%s' % dp_id
+                raise ValueError(msg)
+
+        return dps
+
+class ForwardController(ControllerBase):
+
+    _OFS_LIST = ForwardOfsList()
+    _LOGGER = None
+
+    def __init__(self, req, link, data, **config):
+        super(ForwardController, self).__init__(req, link, data, **config)
+        self.dpset = data['dpset']
+        self.waiters = data['waiters']
+
+    @classmethod
+    def set_logger(cls, logger):
+        cls._LOGGER = logger
+        cls._LOGGER.propagate = False
+        hdlr = logging.StreamHandler()
+        fmt_str = '[Forward][%(levelname)s] %(message)s'
+        hdlr.setFormatter(logging.Formatter(fmt_str))
+        cls._LOGGER.addHandler(hdlr)
+
+    @staticmethod
+    def regist_ofs(dp, CONF):
+        if dp.id in ForwardController._OFS_LIST:
+            return
+
+        dpid_str = dpid_lib.dpid_to_str(dp.id)
+        try:
+            f_ofs = Forward(dp, CONF)
+            f_ofs.set_default_flow()
+        except OFPUnknownVersion as message:
+            ForwardController._LOGGER.info('dpid=%s: %s',
+                                       dpid_str, message)
+            return
+
+        ForwardController._OFS_LIST.setdefault(dp.id, f_ofs)
+        ForwardController._LOGGER.info('dpid=%s: Join forward switch.',
+                                   dpid_str)
+
+    @staticmethod
+    def unregist_ofs(dp):
+        if dp.id in ForwardController._OFS_LIST:
+            del ForwardController._OFS_LIST[dp.id]
+            ForwardController._LOGGER.info('dpid=%s: Leave forward switch.',
+                                       dpid_lib.dpid_to_str(dp.id))
+
+    @staticmethod
+    def set_ovsdb_addr(dpid, value):
+        ofs = ForwardController._OFS_LIST.get(dpid, None)
+        if ofs is not None:
+            ofs.set_ovsdb_addr(dpid, value)
+
+    @staticmethod
+    def delete_ovsdb_addr(dpid):
+        ofs = ForwardController._OFS_LIST.get(dpid, None)
+        ofs.set_ovsdb_addr(dpid, None)
+
+    @route('forward', BASE_URL + '/rules/{switchid}',
+           methods=['GET'], requirements=REQUIREMENTS)
+    def get_rules(self, req, switchid, **_kwargs):
+        return self._access_switch(req, switchid, VLANID_NONE,
+                                   'get_rules', None)
+
+    @route('forward', BASE_URL + '/rules/{switchid}',
+           methods=['POST'], requirements=REQUIREMENTS)
+    def set_rule(self, req, switchid, **_kwargs):
+        return self._access_switch(req, switchid, VLANID_NONE,
+                                   'set_rule', None)
+
+    @route('forward', BASE_URL + '/rules/{switchid}',
+           methods=['DELETE'], requirements=REQUIREMENTS)
+    def delete_rule(self, req, switchid, **_kwargs):
+        return self._access_switch(req, switchid, VLANID_NONE,
+                                   'delete_rule', None)
+
+    @route('forward', BASE_URL + '/rules/status/{switchid}',
+           methods=['GET'], requirements=REQUIREMENTS)
+    def get_status(self, req, switchid, **_kwargs):
+        return self._access_switch(req, switchid, VLANID_NONE,
+                                   'get_status', self.waiters)
+
+    @route('forward', BASE_URL + '/rules/{switchid}/{vlanid}',
+           methods=['GET'], requirements=REQUIREMENTS)
+    def get_vlan_rules(self, req, switchid, vlanid, **_kwargs):
+        return self._access_switch(req, switchid, vlanid,
+                                   'get_rules', self.waiters)
+
+
+    @route('forward', BASE_URL + '/rules/{switchid}/{vlanid}',
+           methods=['POST'], requirements=REQUIREMENTS)
+    def set_vlan_rule(self, req, switchid, vlanid, **_kwargs):
+        return self._access_switch(req, switchid, vlanid,
+                                   'set_rule', self.waiters)
+
+    @route('forward', BASE_URL + '/rules/{switchid}/{vlanid}',
+           methods=['DELETE'], requirements=REQUIREMENTS)
+    def delete_vlan_rule(self, req, switchid, vlanid, **_kwargs):
+        return self._access_switch(req, switchid, vlanid,
+                                   'delete_rule', self.waiters)
+
+    def _access_switch(self, req, switchid, vlan_id, func, waiters):
+        try:
+            rest = req.json if req.body else {}
+        except ValueError:
+            ForwardController._LOGGER.debug('invalid syntax %s', req.body)
+            return Response(status=400)
+
+        try:
+            dps = self._OFS_LIST.get_ofs(switchid)
+            vid = ForwardController._conv_toint_vlanid(vlan_id)
+        except ValueError as message:
+            return Response(status=400, body=str(message))
+
+        msgs = []
+        for f_ofs in dps.values():
+            function = getattr(f_ofs, func)
+            try:
+                if waiters is not None:
+                    msg = function(rest, vid, waiters)
+                else:
+                    msg = function(rest, vid)
+            except ValueError as message:
+                return Response(status=400, body=str(message))
+            msgs.append(msg)
+
+        body = json.dumps(msgs)
+        return Response(content_type='application/json', body=body)
+
+    @staticmethod
+    def _conv_toint_vlanid(vlan_id):
+        if vlan_id != REST_ALL:
+            vlan_id = int(vlan_id)
+            if (vlan_id != VLANID_NONE and
+                    (vlan_id < VLANID_MIN or VLANID_MAX < vlan_id)):
+                msg = 'Invalid {vlan_id} value. Set [%d-%d]' % (VLANID_MIN,
+                                                                VLANID_MAX)
+                raise ValueError(msg)
+        return vlan_id
+
+class Forward(object):
+
+    _OFCTL = {ofproto_v1_0.OFP_VERSION: ofctl_v1_0,
+              ofproto_v1_2.OFP_VERSION: ofctl_v1_2,
+              ofproto_v1_3.OFP_VERSION: ofctl_v1_3}
+
+    def __init__(self, dp, CONF):
+        super(Forward, self).__init__()
+        self.vlan_list = {}
+        self.vlan_list[VLANID_NONE] = 0  # for VLAN=None
+        self.dp = dp
+        self.version = dp.ofproto.OFP_VERSION
+        self.queue_list = {}
+        self.CONF = CONF
+        self.ovsdb_addr = None
+        self.ovs_bridge = None
+
+        if self.version not in self._OFCTL:
+            raise OFPUnknownVersion(version=self.version)
+
+        self.ofctl = self._OFCTL[self.version]
+
+    def set_default_flow(self):
+        if self.version == ofproto_v1_0.OFP_VERSION:
+            return
+
+        cookie = 0
+        priority = DEFAULT_FLOW_PRIORITY
+        actions = [{'type': 'GOTO_TABLE',
+                    'table_id': QOS_TABLE_ID + 1}]
+        flow = self._to_of_flow(cookie=cookie,
+                                priority=priority,
+                                match={},
+                                actions=actions)
+
+        cmd = self.dp.ofproto.OFPFC_ADD
+        self.ofctl.mod_flow_entry(self.dp, flow, cmd)
+
+    def set_ovsdb_addr(self, dpid, ovsdb_addr):
+        # easy check if the address format valid
+        _proto, _host, _port = ovsdb_addr.split(':')
+
+        old_address = self.ovsdb_addr
+        if old_address == ovsdb_addr:
+            return
+        if ovsdb_addr is None:
+            if self.ovs_bridge:
+                self.ovs_bridge.del_controller()
+                self.ovs_bridge = None
+            return
+        self.ovsdb_addr = ovsdb_addr
+        if self.ovs_bridge is None:
+            ovs_bridge = bridge.OVSBridge(self.CONF, dpid, ovsdb_addr)
+            self.ovs_bridge = ovs_bridge
+            try:
+                ovs_bridge.init()
+            except:
+                raise ValueError('ovsdb addr is not available.')
+
+    def _update_vlan_list(self, vlan_list):
+        for vlan_id in self.vlan_list.keys():
+            if vlan_id is not VLANID_NONE and vlan_id not in vlan_list:
+                del self.vlan_list[vlan_id]
+
+    def _get_cookie(self, vlan_id):
+        if vlan_id == REST_ALL:
+            vlan_ids = self.vlan_list.keys()
+        else:
+            vlan_ids = [vlan_id]
+
+        cookie_list = []
+        for vlan_id in vlan_ids:
+            self.vlan_list.setdefault(vlan_id, 0)
+            self.vlan_list[vlan_id] += 1
+            self.vlan_list[vlan_id] &= ofproto_v1_3_parser.UINT32_MAX
+            cookie = (vlan_id << COOKIE_SHIFT_VLANID) + \
+                self.vlan_list[vlan_id]
+            cookie_list.append([cookie, vlan_id])
+
+        return cookie_list
+
+    @staticmethod
+    def _cookie_to_forwardid(cookie):
+        return cookie & ofproto_v1_3_parser.UINT32_MAX
 
 
 
 
-
-    def _set_forward(self, cookie, rest, waiters, vlan_id):
+    def _set_rule(self, cookie, rest, waiters, vlan_id):
         match_value=rest[REST_MATCH]
         if vlan_id:
             match_value[REST_DL_VLAN] = vlan_id
 
-        priority = int(rest.get(REST_PRIORITY, QOS_PRIORITY_MIN))
-        if (QOS_PRIORITY_MAX < priority):
+        priority = int(rest.get(REST_PRIORITY, FORWARD_PRIORITY_MIN))
+        if (FORWARD_PRIORITY_MAX < priority):
             raise ValueError('Invalid priority value. Set [%d-%d]'
-                             % (QOS_PRIORITY_MIN, QOS_PRIORITY_MAX))
+                             % (FORWARD_PRIORITY_MIN, FORWARD_PRIORITY_MAX))
 
         match = Match.to_openflow(match_value)
         actions = []
@@ -153,7 +484,19 @@ LOG = logging.getLogger(__name__)
                         'table_id': QOS_TABLE_ID + 1})
         flow = self._to_of_flow(cookie=cookie, priority=priority,
                                 match=match, actions=actions)  
-        
+        cmd = self.dp.ofproto.OFPFC_ADD
+        try:
+            self.ofctl.mod_flow_entry(self.dp, flow, cmd)
+        except:
+            raise ValueError('Invalid rule parameter.')
+
+        qos_id = QoS._cookie_to_qosid(cookie)
+        msg = {'result': 'success',
+               'details': 'Port forwarding added. : forward_id=%d' % forward_id}
+
+        if vlan_id != VLANID_NONE:
+            msg.setdefault(REST_VLANID, vlan_id)
+        return msg
         
 
 
